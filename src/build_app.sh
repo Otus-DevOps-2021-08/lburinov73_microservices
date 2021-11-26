6f0a176abf39da1662a47ed0be383bab9dd3f7a2
Leonid Burinov <lburinov@simtechdev.com>
2021-11-26 11:53:17 +0400

    docker-3: homework N14 -- small fixes

--- src/build_app.sh
+++ src/build_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull mongo:latest
-docker build -t lburinov73/post:1.0 ./post-py
-docker build -t lburinov73/comment:1.0 ./comment
+docker build -t lburinov73/post:2.0 ./post-py
+docker build -t lburinov73/comment:2.0 ./comment
 docker build -t lburinov73/ui:2.0 ./ui
