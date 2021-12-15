14877284d1bb07eaacc78d011e68344f1aa71011
Leonid Burinov <lburinov@simtechdev.com>
2021-12-15 20:56:37 +0400

    monitoring-1: homework 17 - fix tests

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export USER_NAME=lburinov73
+#export USER_NAME=lburinov73
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
