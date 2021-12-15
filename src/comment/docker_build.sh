949b2def61385a6d1caed816def39ae0a8b987d4
Leonid Burinov <lburinov@simtechdev.com>
2021-12-15 20:51:46 +0400

    monitoring-1: homework 17 - fix tests

--- src/comment/docker_build.sh
+++ src/comment/docker_build.sh
@@ -5,4 +5,4 @@ export USER_NAME=lburinov73
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-docker build -t $USER_NAME/comment:3.0 .
+docker build -t $USER_NAME/comment:latest .
