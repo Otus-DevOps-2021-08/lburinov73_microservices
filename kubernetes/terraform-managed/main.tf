b9db01898f69a3af1f66c6ea3d7033ae773bed78
Leonid Burinov <lburinov@simtechdev.com>
2022-01-24 12:08:54 +0400

    kubernetes-2: homework N22 - initial commit

---
+++ kubernetes/terraform-managed/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_kubernetes_cluster" "reddit" {
+  name        = "reddit-cluster"
+  description = "reddit app cluster"
+
+  network_id = var.network_id
+
+  master {
+    version = var.kubernetes_version
+    zonal {
+      zone      = var.zone
+      subnet_id = var.subnet_id
+    }
+
+    public_ip = true
+
+    maintenance_policy {
+      auto_upgrade = true
+    }
+  }
+
+  service_account_id      = var.service_account_id
+  node_service_account_id = var.service_account_id
+
+  release_channel         = var.kubernetes_release_channel
+}
+
+
+resource "yandex_kubernetes_node_group" "reddit_node_group" {
+  cluster_id  = yandex_kubernetes_cluster.reddit.id
+  name        = "reddit-nodegroup"
+  description = "reddit cluster node group"
+  version     = var.kubernetes_version
+
+  instance_template {
+    platform_id = "standard-v2"
+
+    network_interface {
+      nat        = true
+      subnet_ids = [var.subnet_id]
+    }
+
+    resources {
+      memory = var.res_memory
+      cores  = var.res_cores
+    }
+
+    boot_disk {
+      type = "network-ssd"
+      size = var.res_disk_size
+    }
+
+    scheduling_policy {
+      preemptible = false
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = var.worker_count
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = var.zone
+    }
+  }
+
+  maintenance_policy {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+}
---
+++ kubernetes/terraform-managed/variables.tf
@@ -0,0 +1,46 @@
+variable "cloud_id" {
+  description = "Cloud"
+}
+variable "folder_id" {
+  description = "Folder"
+}
+variable "network_id" {
+  description = "Network"
+}
+variable "zone" {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable "subnet_id" {
+  description = "Subnet"
+}
+variable "service_account_key_file" {
+  description = "key .json"
+}
+variable "service_account_id" {
+  description = "service account id"
+}
+variable "res_cores" {
+  description = "CPU cores for the instance"
+  default     = 4
+}
+variable "res_memory" {
+  description = "Memory size in GB"
+  default     = 8
+}
+variable "worker_count" {
+  description = "Number of worker nodes"
+  default     = 2
+}
+variable "res_disk_size" {
+  description = "Disk size"
+  default     = 64
+}
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  default     = "1.19"
+}
+variable "kubernetes_release_channel" {
+  description = "Kubernetes release channel"
+  default     = "RAPID"
+}
